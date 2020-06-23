82c775fc62074fe18cb2c0e69f9657c1abaf638f
Barnali <mail2barnali1990@gmail.com>
2020-06-23 14:30:03 +0530

    Added JPA entities

---
+++ src/test/java/com/barnali/project1/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barnali.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
